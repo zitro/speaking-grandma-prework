70522e32310fac2b573662e07955904594d0158f
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-09 13:56:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(response)
-  if response == response.uppercase
+  if response == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif response == "".uppercase
     puts "NO, NOT SINCE 1938!"
-  elsif response == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
