a8a8bf924d4263347ba0f74993f9b7d4d5b4573f
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-09 13:57:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(response)
   if response == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif response == "".uppercase
+elsif response == "".upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
